1582d0e1e1ca87d183b90ee1a2a0797e49f4d29b
Carlos Pérez <cperezh@gmail.com>
2013-12-06 21:14:13 +0100

    Primera versión que envía emails

--- src/main/java/com/carlos/avisamedelcoche/Avisador.java
+++ src/main/java/com/carlos/avisamedelcoche/Avisador.java
@@ -5,7 +5,15 @@
  */
 package com.carlos.avisamedelcoche;
 
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 /**
@@ -14,15 +22,38 @@ import javax.mail.internet.MimeMessage;
  */
 class Avisador {
 
-    static void avisarUsuario() {
+    static void avisarUsuario() throws MessagingException {
         String emailUsuario = Usuario.obtenerEmail();
-        
+
         enviarEmail(emailUsuario);
     }
 
-    private static void enviarEmail(String emailUsuario) {
-     MimeMessage mensaje;   
-     Transport transporte;
+    private static void enviarEmail(String emailUsuario) throws MessagingException {
+
+        String msgText = "This is a message body.\nHere's the second line.";
+
+// create some properties and get the default Session
+        Properties mailProps = new Properties();
+        mailProps.put("mail.smtps.host", "smtp.gmail.com");
+        mailProps.put("mail.smtps.port", "465");
+        mailProps.put("mail.transport.protocol", "smtps");
+
+        Session session = Session.getInstance(mailProps, null);
+
+        MimeMessage msg = new MimeMessage(session);
+
+        msg.setFrom(new InternetAddress(emailUsuario));
+        InternetAddress[] address = {new InternetAddress(emailUsuario)};
+        msg.setRecipients(Message.RecipientType.TO, address);
+        msg.setSubject("JavaMail APIs Test");
+        msg.setSentDate(new Date());
+        // If the desired charset is known, you can use
+        // setText(text, charset)
+        msg.setText(msgText);
+
+        Transport tr = session.getTransport();
+        tr.connect("cperezh@gmail.com", "M0t1v377");
+        tr.sendMessage(msg, address);
     }
 
 }
--- src/test/java/com/carlos/avisamedelcoche/AvisadorTest.java
+++ src/test/java/com/carlos/avisamedelcoche/AvisadorTest.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.carlos.avisamedelcoche;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
 import junit.framework.TestCase;
 
 /**
@@ -13,16 +15,16 @@ import junit.framework.TestCase;
  * @author Pakno
  */
 public class AvisadorTest extends TestCase {
-    
+
     public AvisadorTest(String testName) {
         super(testName);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
@@ -31,9 +33,11 @@ public class AvisadorTest extends TestCase {
     /**
      * Test of avisarUsuario method, of class Avisador.
      */
-    public void testAvisarUsuario() {
+    public void testAvisarUsuario() throws MessagingException {
+
         Avisador.avisarUsuario();
         assertTrue(true);
+
     }
-    
+
 }
