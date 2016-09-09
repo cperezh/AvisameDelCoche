55b320e62609e6aee2982290c4c180023518a153
Carlos Pérez Haurie <cperezh@gmail.com>
2016-09-09 07:01:06 +0200

    Backup de la base de datos de Producción.

--- doc/backup_bd/bckup_090916.sql
+++ doc/backup_bd/bckup_090916.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.10, for Win64 (x86_64)
 --
--- Host: localhost    Database: avisamedelcoche
+-- Host: localhost    Database: avisamedelcoche_pro
 -- ------------------------------------------------------
 -- Server version	5.7.10-enterprise-commercial-advanced-log
 
@@ -35,8 +35,35 @@ CREATE TABLE `coche` (
 
 LOCK TABLES `coche` WRITE;
 /*!40000 ALTER TABLE `coche` DISABLE KEYS */;
-INSERT INTO `coche` VALUES ('4731dsd',170900);
+INSERT INTO `coche` VALUES ('4731dsd',176000);
 /*!40000 ALTER TABLE `coche` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estadocomponente`
+--
+
+DROP TABLE IF EXISTS `estadocomponente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estadocomponente` (
+  `COMPONENTE` int(11) NOT NULL,
+  `ULTIMASUSTITUCION` int(11) DEFAULT NULL,
+  `matricula` varchar(255) NOT NULL,
+  PRIMARY KEY (`COMPONENTE`,`matricula`),
+  KEY `fk_estadocomponente_matricula` (`matricula`),
+  CONSTRAINT `fk_estadocomponente_matricula` FOREIGN KEY (`matricula`) REFERENCES `coche` (`MATRICULA`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estadocomponente`
+--
+
+LOCK TABLES `estadocomponente` WRITE;
+/*!40000 ALTER TABLE `estadocomponente` DISABLE KEYS */;
+INSERT INTO `estadocomponente` VALUES (0,160001,'4731dsd'),(1,170865,'4731dsd'),(2,170865,'4731dsd'),(3,170865,'4731dsd'),(4,170865,'4731dsd'),(5,170865,'4731dsd'),(6,160000,'4731dsd'),(7,160000,'4731dsd'),(8,160000,'4731dsd'),(9,160001,'4731dsd');
+/*!40000 ALTER TABLE `estadocomponente` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -48,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-29 22:35:09
+-- Dump completed on 2016-09-09  6:57:08
